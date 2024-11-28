874d30162d0f23d7b5bac3af1e4381b3363a7a5f
rabbit1rabbit <147872988+rabbit1rabbit@users.noreply.github.com>
2024-11-27 22:48:08 -0700

    completed information and css

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ jimenez_pets.addEventListener('click', function(){
     document.body.classList.add('jimenez_pets');
     document.getElementById('pet-pic').src="assets/JimenezPets.jpeg"
     document.getElementById('pet-pic').alt="Picture of three animals in a living room. From left to right, Bagel the beagle, Lola the bunny, Brownie the pitbull."
-    document.getElementsByClassName('information')[0].innerHTML= "These are the three Jimenez family pets";
+    document.getElementsByClassName('information')[0].innerHTML= "These three animals in the picture are the pets of the Jimenez Family. From left to right there is Bagel the beagle, Lola the bunny, and Brownie the pitbull. My family was against the idea of owning pets up until we got our first pet. In 2017 my brother adopted our first pet and ever since then my family has been open to getting more pets.";
 
 }
 )
@@ -15,7 +15,7 @@ lola_btn.addEventListener('click', function(){
     document.body.classList.add('lola');
     document.getElementById('pet-pic').src="assets/LolaBunny.jpeg"
     document.getElementById('pet-pic').alt="Picture of Lola the bunny."
-    document.getElementsByClassName('information')[0].innerHTML= "Lola placeholder text";
+    document.getElementsByClassName('information')[0].innerHTML= "Lola is the first pet that my family got. Around October 2017 my older brother decided to adopt a white Netherland dwarf rabbit. My brother wanted a pet for quite  a while and decided the time was right when the both of us were away from home for university. My parents were surprised that my brother got a pet but they quickly found themselves adoring the bunny. Lola was usually pretty relaxed but during nights she was very energetic and loved to run. Unfortunately Lola passed away in March 2024.";
 
 }
 )
@@ -26,7 +26,7 @@ bagel_btn.addEventListener('click', function(){
     document.body.classList.add('bagel');
     document.getElementById('pet-pic').src="assets/BagelBeagle.jpeg"
     document.getElementById('pet-pic').alt="Picture of Bagel the beagle."
-    document.getElementsByClassName('information')[0].innerHTML= "Bagel placeholder text";
+    document.getElementsByClassName('information')[0].innerHTML= "Bagel was the first dog that our family adopted. Bagel was adopted by my older brother around April 2020. Bagel was one years old when she was adopted. Her previous family had to give her away because they were moving away and could not bring a pet. My brother always wanted a dog and after a couple years of having Lola he decided that he was ready to get another pet. Bagel is a very relaxed dog who loves food and going for walks. She will always sit by someones legs whenever there is food around.";
 
 }
 )
@@ -37,7 +37,7 @@ brownie_btn.addEventListener('click', function(){
     document.body.classList.add('brownie');
     document.getElementById('pet-pic').src="assets/BrowniePitbull.jpeg"
     document.getElementById('pet-pic').alt="Picture of Brownie the pitbull"
-    document.getElementsByClassName('information')[0].innerHTML= "Brownie placeholder text";
+    document.getElementsByClassName('information')[0].innerHTML= "Brownie was adopted around December 2022. We got him when he was just 3 months old. Brownie was given to us by my aunt’s friend when he was just 3 months old. He was a very energetic puppy and always loved to run around. When we first got him he was smaller than Bagel but within 4 months he was already bigger than her. At first I wasn’t very fond of bigger dogs but after spending time with Brownie I can definitely see the appeal in owning a big dog.";
 
 }
 )
